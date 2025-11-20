49152cdb1dd1570fc092c8f0c3cfef30137025ff
Jamila <jibra001@gold.ac.uk>
2025-11-20 01:24:26 +0000

    Added audit log table into the script

--- create_db.sql
+++ create_db.sql
@@ -22,6 +22,14 @@ CREATE TABLE IF NOT EXISTS users (
     PRIMARY KEY(id)
 );
 
+# Create the audit log table
+CREATE TABLE IF NOT EXISTS audit_log (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100),
+    success BOOLEAN,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
 # Create the application user
 CREATE USER IF NOT EXISTS 'berties_books_app'@'localhost' IDENTIFIED BY 'qwertyuiop'; 
 GRANT ALL PRIVILEGES ON berties_books.* TO ' berties_books_app'@'localhost';
